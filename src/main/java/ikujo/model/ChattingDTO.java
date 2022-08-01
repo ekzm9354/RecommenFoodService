c90b09207e1001a194c6851b0fc60c7e2d178754
smhrd <smhrd@61.80.80.212>
2022-08-01 15:24:10 +0900

    chatting 부분 추가

---
+++ src/main/java/ikujo/model/ChattingDTO.java
@@ -0,0 +1,21 @@
+package ikujo.model;
+
+import java.sql.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@RequiredArgsConstructor
+public class ChattingDTO {
+	private Date c_date;
+//	받는 사람
+	@NonNull private String toName;
+//	보내는 사람
+	@NonNull private String fromName;
+	private String messeges;
+	
+}
